c341a7dcf44e9260fd08ed9e47d69a45f1ad01be
shannon hart <shannonehart18@gmail.com>
2018-01-13 13:42:14 -0600

    added the index css and js files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+   apiKey: "AIzaSyCV5hb3_Ky3kv8z_N0L63A4-fNMmJHKnNM",
+   authDomain: "travel-fund.firebaseapp.com",
+   databaseURL: "https://travel-fund.firebaseio.com",
+   projectId: "travel-fund",
+   storageBucket: "",
+   messagingSenderId: "959467319333"
+ };
+ firebase.initializeApp(config);
+
+  var database = firebase.database();
